18881d5806370a5f0f5c8fd1d855706e383052d9
pravtz <ederson.ufrgs@gmail.com>
2023-09-13 22:27:43 -0300

    feat: Search screen

--- src/components/ScreenSearch/index.tsx
+++ src/components/ScreenSearch/index.tsx
@@ -1,18 +1,82 @@
 'use client'
 
-import {Wrapper} from "./styled";
+import {GroupCards, Wrapper} from "./styled";
 import {NavigationLink} from "@/components/NavigationLink";
 import {InputSearch} from "@/components/InputSearch";
+import {getListUsers} from "@/services/api.github.com/getListUsers";
+import {MenuLine} from "@/components/MenuLine";
+import {CardSearch} from "@/components/CardSearch";
+import {boolean} from "zod";
+
+
+const mocktest = [
+
+    {
+        name: "Ederson Oliveira Pravtz",
+        login: "pravtz",
+        location: "Brasil",
+        image: {
+            src:"https://avatars.githubusercontent.com/u/32251704?v=4",
+            alt: "foto Ederson"
+        }
+
+    },
+    {
+        name: "Ederson Oliveira Pravtz",
+        login: "pravtz",
+        location: "Brasil",
+        image: {
+            src:"https://avatars.githubusercontent.com/u/32251704?v=4",
+            alt: "foto Ederson"
+        }
+
+    },{
+        name: "Ederson Oliveira Pravtz",
+        login: "pravtz",
+        location: "Brasil",
+        image: {
+            src:"https://avatars.githubusercontent.com/u/32251704?v=4",
+            alt: "foto Ederson"
+        }
+
+    }
+
+]
 
 export const ScreenSearch = () => {
     const handlerSearch = async (data: string) => {
-        console.log(data)
-    }
+        //const dataAll = await getListUsers(data,1,3,"desc", "joined")
 
+    }
+    const arrayRecentSearches = mocktest
+    const arrayCurrentSearches = mocktest
+
+    const amountCards = arrayRecentSearches.length
     return (
         <Wrapper>
             <NavigationLink name="Sair" href="/" />
             <InputSearch handlerSearchUsers={handlerSearch}/>
+
+            <MenuLine isEmpty={!amountCards} title={"Buscas Recentes"}>
+                <GroupCards >
+                {arrayRecentSearches.map((item,index)=>{
+                    return (
+                            <CardSearch key={index} name={item.name} login={item.login} location={item.location} image={item.image}/>
+                    )
+                })}
+                </GroupCards>
+            </MenuLine>
+
+            <MenuLine isEmpty={!amountCards} title={"Buscas"} isStartOpen={true}>
+                <GroupCards >
+                    {arrayCurrentSearches.map((item,index)=>{
+                        return (
+                            <CardSearch key={index} name={item.name} login={item.login} location={item.location} image={item.image}/>
+                        )
+                    })}
+                </GroupCards>
+            </MenuLine>
+
         </Wrapper>
     )
 }
--- src/components/ScreenSearch/styled.tsx
+++ src/components/ScreenSearch/styled.tsx
@@ -3,4 +3,11 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 16px;
+`
+
+export const GroupCards = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
 `
