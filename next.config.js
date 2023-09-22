80f4753b18af314e32772c56e55aaf592e26257a
pravtz <ederson.ufrgs@gmail.com>
2023-09-22 15:36:49 -0300

    add sass configurations and remove styled-components configurations

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
     reactStrictMode: true,
-    compiler: {
-        styledComponents: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
     },
     images:{
         domains: [
